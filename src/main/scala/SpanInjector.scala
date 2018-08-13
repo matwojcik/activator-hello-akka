24a3f305271078176600d11b1b46ed811f0d43e0
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2018-08-13 13:24:19 +0200

    Creating span if not exists

--- src/main/scala/SpanInjector.scala
+++ src/main/scala/SpanInjector.scala
@@ -1,7 +1,8 @@
+import io.opentracing.util.GlobalTracer
+
 object SpanInjector {
   def injectSpanToMdc= {
-    val span = Option(currentSpan)
-    span.foreach { span =>
+    val span = Option(currentSpan) getOrElse createSpan
       val traceId = span.asInstanceOf[com.uber.jaeger.Span].context().getTraceId.toString
       val spanId = span.asInstanceOf[com.uber.jaeger.Span].context().getSpanId.toString
       if (traceId ne null)
@@ -9,6 +10,10 @@ object SpanInjector {
       if (spanId ne null)
         org.slf4j.MDC.put("X-B3-SpanId", spanId)
     }
+
+  private def createSpan = {
+    val tracer = GlobalTracer.get
+    tracer.buildSpan("custom-span").startActive(false).span()
   }
 
   private def currentSpan = {
